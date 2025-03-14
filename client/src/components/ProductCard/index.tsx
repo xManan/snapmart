87a146aa396191fd4846afb936b628b015f4497b
Manan Chawla <mananchawla10@gmail.com>
2025-03-14 17:48:02 +0530

    update

--- client/src/components/ProductCard/index.tsx
+++ client/src/components/ProductCard/index.tsx
@@ -14,7 +14,6 @@ function ProductCard({ product }: ProductCardProps) {
     const cartItems = useCartStore(state => state.items)
     const addItem = useCartStore(state => state.addItem)
     const removeItem = useCartStore(state => state.removeItem)
-    const [ imgSrc, setImgSrc ] = useState<string>(product.product_img_path)
     const handleAddToCart = () => {
         addItem({
             product_id: product.product_id,
